b2a4d2a14655eb4fa92a87dc9113b7c79c192ca3
RMuskovets <rmuskovets@gmail.com>
2020-05-29 18:03:10 +0300

    use 2 spaces as indent

--- main.lua
+++ main.lua
@@ -1,52 +1,52 @@
 local OPS_TABLE = {
-	plus = "__add",
-	minus = "__sub",
-	times = "__mul",
-	divide = "__div",
-	power = "__pow",
-	modulo = "__mod",
-	idivide = "__idiv",
-	equals = "__eq",
-	lessthan = "__lt",
-	lesseqthan = "__le",
+  plus = "__add",
+  minus = "__sub",
+  times = "__mul",
+  divide = "__div",
+  power = "__pow",
+  modulo = "__mod",
+  idivide = "__idiv",
+  equals = "__eq",
+  lessthan = "__lt",
+  lesseqthan = "__le",
 
-	binand = "__band",
-	binor = "__bor",
-	binxor = "__bxor",
-	binnot = "__bnot",
-	shiftl = "__bshl",
-	shiftr = "__bshr",
+  binand = "__band",
+  binor = "__bor",
+  binxor = "__bxor",
+  binnot = "__bnot",
+  shiftl = "__bshl",
+  shiftr = "__bshr",
 
-	length = "__len",
-	concat = "__concat",
+  length = "__len",
+  concat = "__concat",
 
-	call = "__call",
-	tostring = "__tostring"
+  call = "__call",
+  tostring = "__tostring"
 }
 
 return function (class, parent)
 
-	local new = class.constructor or function (self) end
-	
-	local nclass = { super = parent }
-	nclass.__index = nclass
+  local new = class.constructor or function (self) end
+  
+  local nclass = { super = parent }
+  nclass.__index = nclass
 
-	for k, v in pairs(parent or {}) do
-		if k ~= "new" and k ~= "super" then
-			nclass[k] = v
-		end
-	end
+  for k, v in pairs(parent or {}) do
+    if k ~= "new" and k ~= "super" then
+      nclass[k] = v
+    end
+  end
 
-	for key, value in pairs(class) do
-		nclass[OPS_TABLE[key] or key] = value
-	end
+  for key, value in pairs(class) do
+    nclass[OPS_TABLE[key] or key] = value
+  end
 
-	function nclass:new(...)
-		local obj = { __class = nclass, super = parent }
-		new(obj, ...)
-		setmetatable(obj, nclass)
-		return obj
-	end
+  function nclass:new(...)
+    local obj = { __class = nclass, super = parent }
+    new(obj, ...)
+    setmetatable(obj, nclass)
+    return obj
+  end
 
-	return nclass
+  return nclass
 end
--- test.lua
+++ test.lua
@@ -4,15 +4,15 @@ local Str
 
 Str = class {
 
-	constructor = function (self, s) self.__str = s end,
+  constructor = function (self, s) self.__str = s end,
 
-	plus = function (lhs, rhs) return lhs.__class:new(tostring(lhs) .. tostring(rhs)) end,
-	times = function (lhs, rhs) return lhs.__class:new(string.rep(tostring(lhs), tonumber(rhs))) end,
-	concat = function (lhs, rhs) return lhs + rhs end,
+  plus = function (lhs, rhs) return lhs.__class:new(tostring(lhs) .. tostring(rhs)) end,
+  times = function (lhs, rhs) return lhs.__class:new(string.rep(tostring(lhs), tonumber(rhs))) end,
+  concat = function (lhs, rhs) return lhs + rhs end,
 
-	length = function (self) return #self.__str end,
+  length = function (self) return #self.__str end,
 
-	tostring = function (self)  return self.__str end
+  tostring = function (self)  return self.__str end
 }
 
 local s = Str:new("hello, world")
@@ -28,12 +28,12 @@ print(#s)
 print(#s2)
 
 local Str2 = class({
-	constructor = function (self, s) self.super.constructor(self, s) end
+  constructor = function (self, s) self.super.constructor(self, s) end
 }, Str)
 
 print(Str:new('world'))
 print(Str2:new('world'))
 
 print(Str2:new("hello, ")
-	 + Str2:new("world") 
-	 + "!!!")
+    + Str2:new("world") 
+    + "!!!")
